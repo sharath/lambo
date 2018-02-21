b05be710eb5aca0a1a4a476f7e4cfd8c45689781
sharath <sharathramku@umass.edu>
2018-02-20 22:50:51 -0500

    using marshal -> unmarshal to automate setting extern to intern

--- controllers/mongo.go
+++ controllers/mongo.go
@@ -4,31 +4,42 @@ import (
 	"gopkg.in/mgo.v2"
 	"github.com/sharath/lambo/models/extern/CMC"
 	"github.com/sharath/lambo/models/intern"
+	"encoding/json"
+	"fmt"
 )
 
 type MongoUpdater struct {
 	db *mgo.Database
+	lim int
 }
 
 func StartMongoUpdater(db *mgo.Database, lim int) *MongoUpdater {
 	m := new(MongoUpdater)
 	m.db = db
+	m.lim = lim
 	go m.start()
 	return m
 }
 
 func (m *MongoUpdater) start() {
 	// every time there's an update from poller
+	var me intern.MongoEntry
+
+	var global *CMC.GlobalData
+	var entries CMC.Entries
 	for range NewPoller().Update {
-		var me intern.MongoEntry
-		var glob *CMC.GlobalData
-		glob.Update()
-		me.Global.TotalMarketCapUsd = glob.TotalMarketCapUsd
-		me.Global.Total24HVolumeUsd = glob.Total24HVolumeUsd
-		me.Global.BitcoinPercentageOfMarketCap = glob.BitcoinPercentageOfMarketCap
-		me.Global.ActiveCurrencies = glob.ActiveCurrencies
-		me.Global.ActiveAssets = glob.ActiveAssets
-		me.Global.ActiveMarkets = glob.ActiveMarkets
-		me.Global.LastUpdated = glob.LastUpdated
+		// get values
+		entries.FetchEntries(m.lim)
+		global.FetchStats()
+
+		// set intern values to extern ones
+		t, _ := json.Marshal(global)
+		json.Unmarshal(t, &me.Global)
+
+		t, _ = json.Marshal(entries)
+		json.Unmarshal(t, &me.Tokens)
+
+		// Let's make sure this works
+		fmt.Println(me)
 	}
 }
