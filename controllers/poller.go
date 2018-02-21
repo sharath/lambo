40f61845147f5814bd9b246c4d83c93acd363dce
sharath <sharathramku@umass.edu>
2018-02-20 23:02:15 -0500

    renamed for consistency

--- controllers/poller.go
+++ controllers/poller.go
@@ -15,7 +15,7 @@ func NewPoller() *Poller {
 	p := new(Poller)
 	p.gdata = new(CMC.GlobalData)
 	p.Update = make(chan int)
-	p.gdata.Update()
+	p.gdata.FetchStats()
 	p.last = p.gdata.LastUpdated
 	go p.start()
 	return p
@@ -24,7 +24,7 @@ func NewPoller() *Poller {
 func (p *Poller) start() {
 	// check every 10 seconds
 	for range time.NewTicker(time.Second * 10).C {
-		p.gdata.Update()
+		p.gdata.FetchStats()
 		if p.gdata.LastUpdated != p.last {
 			p.last = p.gdata.LastUpdated
 			p.Update <- p.last
