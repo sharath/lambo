111c67087d64a55766c97d69075e4829ef0038a9
sharath <sharathramku@umass.edu>
2018-02-20 22:16:34 -0500

    made Update a channel to allow iteration

--- controllers/poller.go
+++ controllers/poller.go
@@ -6,28 +6,26 @@ import (
 )
 
 type Poller struct {
-	last int
-	gdata *CMC.GlobalData
+	last   int
+	gdata  *CMC.GlobalData
+	Update chan int
 }
 
 func NewPoller() *Poller {
 	p := new(Poller)
+	p.gdata = new(CMC.GlobalData)
+	p.Update = make(chan int)
 	p.gdata.Update()
 	p.last = p.gdata.LastUpdated
+	go p.start()
 	return p
 }
 
-func (p *Poller) GetNextUpdate() bool {
-	updated := make(chan bool)
-	poll := func() {
-		for range time.NewTicker(time.Minute).C {
-			p.gdata.Update()
-			if p.gdata.LastUpdated != p.last {
-				p.last = p.gdata.LastUpdated
-				updated <- true
-			}
+func (p *Poller) start() {
+	for range time.NewTicker(time.Minute).C {
+		if p.gdata.LastUpdated != p.last {
+			p.last = p.gdata.LastUpdated
+			p.Update <- p.last
 		}
 	}
-	go poll()
-	return <- updated
 }
