b1fd723e3022b4f96f0a7653a9100cb286fc8659
sharath <sharathramku@umass.edu>
2018-02-23 21:47:36 -0500

    limit to 5 active sessions

--- models/intern/User.go
+++ models/intern/User.go
@@ -4,5 +4,5 @@ type User struct {
 	ID       string   `json:"id" bson:"id"`
 	Username string   `json:"username" bson:"username"`
 	Password string   `json:"username" bson:"password"`
-	AuthKey  []string `json:"auth_key" json:"auth_key"`
+	AuthKey  [5]string `json:"auth_key" json:"auth_key"`
 }
