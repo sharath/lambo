3baef4323f45ced08b4e4e11c2e76a570a9ff90f
sharath <sharathramku@umass.edu>
2018-02-26 01:36:15 -0500

    added humanize

--- lambo.go
+++ lambo.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"github.com/dustin/go-humanize"
 	"github.com/gin-gonic/gin"
 	"github.com/sharath/lambo/controllers"
 	"github.com/sharath/lambo/models/intern"
 	"github.com/sharath/lambo/util"
 	"gopkg.in/mgo.v2"
 	"net/http"
+	"time"
 )
 
 var database *mgo.Database
@@ -80,7 +82,7 @@ func authenticate(context *gin.Context) {
 	p := context.PostForm("password")
 	id, authKey, err := intern.AuthenticateUser(u, p, database.C("users"))
 	if err != nil {
-		forcelogin(context)
+		forceLogin(context)
 		return
 	}
 	auth := &http.Cookie{Name: "auth", Value: authKey, HttpOnly: false}
@@ -93,18 +95,18 @@ func authenticate(context *gin.Context) {
 func dashboard(context *gin.Context) {
 	authKey, err := context.Cookie("auth")
 	if err != nil {
-		forcelogin(context)
+		forceLogin(context)
 		return
 	}
 	id, err := context.Cookie("id")
 	if err != nil {
-		forcelogin(context)
+		forceLogin(context)
 		return
 	}
 
 	valid, err := intern.VerifyAuthKey(id, authKey, database.C("users"))
 	if err != nil || !valid {
-		forcelogin(context)
+		forceLogin(context)
 		return
 	}
 
@@ -115,21 +117,24 @@ func dashboard(context *gin.Context) {
 	if e.Global != nil {
 		g = *e.Global
 	} else {
-		context.Writer.Write([]byte("please wait until first entry is fetched"))
+		context.Writer.Write([]byte("Please wait until first entry is fetched. Usually takes about 5 minutes."))
 		return
 	}
+	NumberOfEntries, _ := database.C("entries").Count()
+	update := time.Unix(int64(g.LastUpdated), 0)
 	context.HTML(http.StatusOK, "dashboard.tmpl", gin.H{
-		"TotalMarketCapUsd":            g.TotalMarketCapUsd,
-		"Total24HVolumeUsd":            g.Total24HVolumeUsd,
+		"TotalMarketCapUsd":            humanize.Commaf(g.TotalMarketCapUsd),
+		"Total24HVolumeUsd":            humanize.Commaf(g.Total24HVolumeUsd),
 		"BitcoinPercentageOfMarketCap": g.BitcoinPercentageOfMarketCap,
 		"ActiveCurrencies":             g.ActiveCurrencies,
 		"ActiveAssets":                 g.ActiveAssets,
 		"ActiveMarkets":                g.ActiveMarkets,
-		"LastUpdated":                  g.LastUpdated,
+		"LastUpdated":                  humanize.Time(update),
+		"NumberOfEntries":              NumberOfEntries,
 	})
 }
 
-func forcelogin(context *gin.Context) {
+func forceLogin(context *gin.Context) {
 	context.HTML(http.StatusUnauthorized, "login.tmpl", gin.H{
 		"unauthorized":   true,
 		"justregistered": false,
