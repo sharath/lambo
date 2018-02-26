cd4508ebbb3af6b9a809373a64208eb23e3cdfcf
sharath <sharathramku@umass.edu>
2018-02-25 20:26:39 -0500

    registration only on intialrun, added debug vs release mode

--- lambo.go
+++ lambo.go
@@ -7,9 +7,11 @@ import (
 	"github.com/sharath/lambo/util"
 	"gopkg.in/mgo.v2"
 	"net/http"
+	"fmt"
 )
 
 var database *mgo.Database
+var initalRun bool
 
 func main() {
 	s, err := mgo.Dial("localhost")
@@ -18,33 +20,43 @@ func main() {
 		util.HandleError(err, true)
 	}
 	database = s.DB("lambo")
+	count, _ := database.C("users").Count()
+	if count < 1 {
+		initalRun = true
+	}
 	lim := 25
 
 	go controllers.StartMongoUpdater(database, lim)
+
+	//gin.SetMode(gin.ReleaseMode)
+	gin.SetMode(gin.DebugMode)
 
 	router := gin.Default()
 	router.LoadHTMLGlob("views/templates/*")
 	router.Static("/static", "views/static")
 	router.GET("/", login)
-	router.GET("/register", getRegister)
 	router.POST("/authenticate", authenticate)
 	router.POST("/register", register)
-	router.Run()
+	router.Run(":80")
 }
 
 func login(context *gin.Context) {
-	context.HTML(http.StatusOK, "login.tmpl", gin.H{
-		"title": "Login",
-	})
-}
-
-func getRegister(context *gin.Context) {
-	context.HTML(http.StatusOK, "register.tmpl", gin.H{
-		"title": "Register",
-	})
+	if !initalRun {
+		context.HTML(http.StatusOK, "login.tmpl", gin.H{
+			"title": "Login",
+		})
+	} else {
+		context.HTML(http.StatusOK, "register.tmpl", gin.H{
+			"title": "Register",
+		})
+	}
 }
 
 func register(context *gin.Context) {
+	if !initalRun {
+		context.JSON(http.StatusUnauthorized, util.NewUnauthorizedResponse())
+		return
+	}
 	u := context.PostForm("username")
 	p := context.PostForm("password")
 	user, err := intern.CreateUser(u, p, database.C("users"))
@@ -66,4 +78,17 @@ func authenticate(context *gin.Context) {
 	context.JSON(http.StatusAccepted, gin.H{
 		"key": authKey,
 	})
+
+	cookie1 := &http.Cookie{Name: "sample", Value: "sample", HttpOnly: false}
+	http.SetCookie(context.Writer, cookie1)
+	fmt.Println(context.Request.Cookie("sample"))
+}
+
+func dashboard(context *gin.Context) {
+	u, err := context.Request.Cookie("auth")
+	if err != nil {
+		login(context)
+		return
+	}
+	fmt.Println(u.Value)
 }
