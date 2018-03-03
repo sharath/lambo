1e07b267e4aa3bc7aed0e2c5ad6fc11734531a0f
sharath <sharathramku@umass.edu>
2018-03-03 00:57:44 -0500

    channels, listens, and pausing/resuming

--- controllers/Poller.go
+++ controllers/Poller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"github.com/sharath/lambo/models/extern/CMC"
 	"time"
+	"fmt"
 )
 
 // Poller is a struct for handling updates on CMC
@@ -10,26 +11,45 @@ type Poller struct {
 	last   int
 	gdata  *CMC.GlobalData
 	Update chan int
+	Resume chan struct{}
+	Pause  chan struct{}
+	paused bool
 }
 
 // StartPoller initializes the poller and returns it
-func StartPoller() *Poller {
+func NewPoller() *Poller {
 	p := new(Poller)
 	p.gdata = new(CMC.GlobalData)
 	p.Update = make(chan int)
+	p.Resume = make(chan struct{})
+	p.Pause = make(chan struct{})
 	p.gdata = CMC.FetchStats()
 	p.last = p.gdata.LastUpdated
 	go p.start()
+	go p.listen()
 	return p
+}
+
+func (p *Poller) listen() {
+	select {
+	case <-p.Resume:
+		fmt.Println("Resuming")
+		p.paused = false
+	case <-p.Pause:
+		fmt.Println("Pausing")
+		p.paused = true
+	}
 }
 
 func (p *Poller) start() {
 	// check every 10 seconds
 	for range time.NewTicker(time.Second * 10).C {
-		p.gdata = CMC.FetchStats()
-		if p.gdata.LastUpdated != p.last {
-			p.last = p.gdata.LastUpdated
-			p.Update <- p.last
+		if !p.paused {
+			p.gdata = CMC.FetchStats()
+			if p.gdata.LastUpdated != p.last {
+				p.last = p.gdata.LastUpdated
+				p.Update <- p.last
+			}
 		}
 	}
 }
