4c717d65693be832cbdd5281ceded344418e9574
sharath <sharathramku@umass.edu>
2018-03-13 23:28:33 -0400

    response format

--- response/Hist.go
+++ response/Hist.go
@@ -1,7 +1,34 @@
 package response
 
-type HistToken struct {
-}
+import (
+	"encoding/json"
+	"github.com/sharath/lambo/database"
+	"gopkg.in/mgo.v2"
+)
 
 type Hist struct {
+	Name           string `json:"name" bson:"name"`
+	Symbol         string `json:"symbol" bson:"symbol"`
+	PriceUsd       string `json:"price_usd" bson:"price_usd"`
+	PriceBtc       string `json:"price_btc" bson:"price_btc"`
+	Two4HVolumeUsd string `json:"24h_volume_usd" bson:"24h_volume_usd"`
+	MarketCapUsd   string `json:"market_cap_usd" bson:"market_cap_usd"`
+	LastUpdated    string `json:"last_updated" bson:"last_updated"`
+}
+
+func NewHist(name string, entries *mgo.Collection) []Hist {
+	var all []*database.MongoEntry
+	var hist []Hist
+	entries.Find(nil).All(&all)
+	for _, e := range all {
+		for _, t := range e.Tokens {
+			if t.Name == name {
+				var temp Hist
+				enc, _ := json.Marshal(t)
+				json.Unmarshal(enc, &temp)
+				hist = append(hist, temp)
+			}
+		}
+	}
+	return hist
 }
