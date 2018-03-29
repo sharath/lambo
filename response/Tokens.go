dec3cd5bdd39fef1dcb68672465b0acebbee2bd8
sharath <sharathramku@umass.edu>
2018-03-29 15:42:56 -0400

    list of tokens

---
+++ response/Tokens.go
@@ -0,0 +1,21 @@
+package response
+
+import (
+	"github.com/sharath/lambo/database"
+	"gopkg.in/mgo.v2"
+)
+
+type Tokens []string
+
+func NewTokens(entries *mgo.Collection, size int) Tokens {
+	var all []*database.MongoEntry
+	entries.Find(nil).All(&all)
+	var t Tokens
+	if len(all) < 1 {
+		return t
+	}
+	for _, i := range all[0].Tokens {
+		t = append(t, i.Name)
+	}
+	return t
+}
